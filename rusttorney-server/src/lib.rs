1810f7f91f29c4ed6885baaae686413d053949b2
Kirill Mironov <k.mironov@albato.ru>
2020-07-25 15:10:46 +0300

    MusicList: resolves #24

--- rusttorney-server/src/lib.rs
+++ rusttorney-server/src/lib.rs
@@ -7,6 +7,7 @@ pub mod client_manager;
 pub mod command;
 pub mod config;
 pub mod master_server_client;
+pub mod music_list;
 pub mod networking;
 pub mod server;
 
--- rusttorney-server/src/server.rs
+++ rusttorney-server/src/server.rs
@@ -9,6 +9,7 @@ use futures::{SinkExt, StreamExt};
 use std::sync::Arc;
 use tokio::net::{TcpListener, TcpStream};
 
+use crate::music_list::MusicList;
 use crate::prompt;
 use futures::channel::mpsc;
 use futures::channel::oneshot::{channel, Receiver, Sender};
@@ -24,6 +25,7 @@ use tokio_util::codec::{Decoder, Framed};
 
 pub struct AOServer {
     config: Arc<Config>,
+    music_list: Arc<MusicList>,
     db: DbWrapper,
     client_manager: Arc<Mutex<ClientManager>>,
 }
@@ -44,12 +46,12 @@ impl AO2MessageHandler {
         mut socket: Framed<TcpStream, AOMessageCodec>,
         db: DbWrapper,
         client_manager: Arc<Mutex<ClientManager>>,
-        timeout: u64,
         timeout_tx: Sender<()>,
         ip: IpAddr,
         config: Arc<Config>,
     ) -> Result<Self, anyhow::Error> {
         let (ch_tx, mut ch_rx) = futures::channel::mpsc::channel(1);
+        let timeout = config.timeout as u64;
 
         tokio::spawn(async move {
             let mut delay =
@@ -172,6 +174,9 @@ impl AO2MessageHandler {
 impl AOServer {
     pub fn new(config: Arc<Config>, db: DbWrapper) -> anyhow::Result<Self> {
         let playerlimit = config.general.playerlimit;
+        let music_list_str = std::fs::read_to_string("./config/music.toml")?;
+        let music_list: Arc<MusicList> =
+            Arc::new(toml::from_str(&music_list_str)?);
         Ok(Self {
             config,
             db: db.clone(),
@@ -179,6 +184,7 @@ impl AOServer {
                 playerlimit,
                 db,
             ))),
+            music_list,
         })
     }
 
@@ -270,7 +276,6 @@ impl AOServer {
                     framed,
                     db,
                     client_manager,
-                    timeout,
                     timeout_tx,
                     c.ip(),
                     config,
